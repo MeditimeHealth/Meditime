121147673e2a7d59bb5826b780dbc3d37b08ade9
David <sakibabdurrahman403@gmail.com>
2026-01-12 16:30:14 +0600

    Update services-section.tsx

--- components/services-section.tsx
+++ components/services-section.tsx
@@ -46,7 +46,7 @@ export default function ServicesSection() {
         <h2
           className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 text-center text-primary leading-tight"
         >
-          Our Services – You are the only one we care about
+          Meditime Services
         </h2>
       </motion.div>
 
