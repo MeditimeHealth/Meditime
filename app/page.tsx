46406733049af97a07d8fa9c016ab86dcec85352
thsvo <155262940+thsvo@users.noreply.github.com>
2026-01-21 08:16:35 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,6 @@ export default function Home() {
       <SearchSection />
       <ServicesSection />
       <WhyChooseSection />
-      
       <DepartmentSection />
       <MembershipSection />
       <StatsSection />
